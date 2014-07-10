383ba1f829f2881c04e32c3c59fd998df459ebc3
Markus Schuss <markus.schuss@student.tugraz.at>
2014-07-10 15:16:12 +0200

    Initial commit for accelerometer test

---
+++ app/src/main/java/at/schuschu/android/spsactivitymonitor/GraphActivity.java
@@ -0,0 +1,87 @@
+package at.schuschu.android.spsactivitymonitor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import android.content.Context;
+
+import android.widget.TextView;
+
+public class GraphActivity extends Activity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private float AxisX,AxisY,AxisZ;
+    private final float ALPHA=0.0f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+
+        AxisX=0;
+        AxisY=0;
+        AxisZ=0;
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+// can be safely ignored for this demo
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.graph, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        TextView tvX= (TextView)findViewById(R.id.x_axis);
+        TextView tvY= (TextView)findViewById(R.id.y_axis);
+        TextView tvZ= (TextView)findViewById(R.id.z_axis);
+
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+        AxisX=(ALPHA * AxisX + (1-ALPHA) * x)*0.5f;
+        AxisY=(ALPHA * AxisY + (1-ALPHA) * y)*0.5f;
+        AxisZ=(ALPHA * AxisZ + (1-ALPHA) * z)*0.5f;
+
+            tvX.setText(Float.toString(AxisX));
+            tvY.setText(Float.toString(AxisY));
+            tvZ.setText(Float.toString(AxisZ));
+
+
+    }
+}
